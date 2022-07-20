1716a2eeadfe28b49167d84f2b66b876b00143a4
Akira Hayashi <smxshxishxad@me.com>
2022-07-21 01:03:31 +0900

    :sparkles: Enable seach in 404.php

--- pacificmall/pacificmall/404.php
+++ pacificmall/pacificmall/404.php
@@ -7,9 +7,9 @@
                       お探しのページは、移動または削除された可能性があります。<br />
                       サイト内検索、または下部フッターリンクより目的のページをお探しください。
                     </p>
-                    <form class="search-form" role="search" method="get" action="">
+                    <form class="search-form" role="search" method="get" action="<?php echo esc_url( home_url() ); ?>">
                       <div class="search-box">
-                        <input type="text" name="" class="search-input" placeholder="キーワードを入力してください" value="">
+                        <input type="text" name="s" class="search-input" placeholder="キーワードを入力してください" value="">
                         <button type="submit" class="button button-submit">検索</button>
                       </div>
                     </form>
